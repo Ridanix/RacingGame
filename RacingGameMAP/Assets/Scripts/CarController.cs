265559ef8c0149d26a986064e76ea57e2ba3fd78
Ridanix <marekmm04@gmail.com>
2023-12-24 23:17:22 +0100

    Vystup 2 Big Update
    Overall:
    	-Added TMP
    Ingame mechanics:
    	-Added Gears (Correctly to real life Tatuus - 6)
    	-Added RPM (Correctly to real life Tatuus - Redline at 6.100 RPM - Idling at 1.470 RPM)
    	-Added Clutch [LeftShift to engage]
    	-Added Gearchanges[E to Upshift, Q to Downshift]
    	-
    Ingame prop changes:
    	-Added Skoda Favorit
    	-Spawn Zone made bigger
    Bunch of QOL changes

--- RacingGameMAP/Assets/Scripts/CarController.cs
+++ RacingGameMAP/Assets/Scripts/CarController.cs
@@ -1,26 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class CarController : MonoBehaviour
 {
     private Rigidbody carRigidBody;
     public WheelColliders wheelColliders;
     public WheelMeshes wheelMeshes;
     public AnimationCurve steeringCurve;
+    public AnimationCurve hpToCurrentRPMCurve;
+    public int currentGear;
     public float brakePower;
     public float brakeInput;
     public float slipAngle;
     public float gasInput;
     public float steeringInput;
     public float enginePower;
+    public float currentRPM;
+    public float redLineRPM;
+    public float idleRPM;
+    public float[] gearRatios;
+    public float carDifferentialRatio;
     private float speed;
+    private float currentTorque;
+    private float carClutch;
+    private float wheelRPM;
+    public TMP_Text currentGearText;
+    public TMP_Text currentRPMText;
     private void Start()
     {
         carRigidBody = gameObject.GetComponent<Rigidbody>();
     }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Q) && currentGear > 0)
+        {
+            currentGear = currentGear - 1;
+        }
+        if (Input.GetKeyDown(KeyCode.E) && currentGear < 5)
+        {
+
+            currentGear = currentGear + 1;
+        }
+        if (currentRPM > redLineRPM) currentRPMText.color = Color.red;
+        else currentRPMText.color = Color.white;
+
+
+        currentGearText.text = (currentGear + 1).ToString();
+        currentRPMText.text = currentRPM.ToString("0,000");
         float steeringAngle = steeringInput * steeringCurve.Evaluate(speed);
         GetInputs();
         ApplyWheels(wheelColliders.LFWheel, wheelMeshes.LFWheel);
@@ -28,8 +55,12 @@ public class CarController : MonoBehaviour
         ApplyWheels(wheelColliders.RFWheel, wheelMeshes.RFWheel);
         ApplyWheels(wheelColliders.RRWheel, wheelMeshes.RRWheel);
         speed = carRigidBody.velocity.magnitude;
-        wheelColliders.LRWheel.motorTorque = enginePower * gasInput;
-        wheelColliders.RRWheel.motorTorque = enginePower * gasInput;
+
+        currentTorque = CalculateTorque();
+        wheelColliders.LRWheel.motorTorque = currentTorque * gasInput;
+        wheelColliders.RRWheel.motorTorque = currentTorque * gasInput;
+
+
         wheelColliders.LFWheel.steerAngle = steeringAngle;
         wheelColliders.RFWheel.steerAngle = steeringAngle;
         wheelColliders.LFWheel.brakeTorque = brakePower * brakeInput * 0.75f;
@@ -37,8 +68,27 @@ public class CarController : MonoBehaviour
         wheelColliders.LRWheel.brakeTorque = brakePower * brakeInput * 0.35f;
         wheelColliders.RRWheel.brakeTorque = brakePower * brakeInput * 0.35f;
     }
+
+    float CalculateTorque()
+    {
+        float torque = 0;
+
+        if (carClutch < 0.1f)
+        {
+            currentRPM = Mathf.Lerp(currentRPM, Mathf.Max(idleRPM, redLineRPM * gasInput) + Random.Range(-50, 50), Time.deltaTime);
+        }
+        else
+        {
+            wheelRPM = Mathf.Abs((wheelColliders.RRWheel.rpm + wheelColliders.LRWheel.rpm) / 2f) * gearRatios[currentGear] * carDifferentialRatio;
+            currentRPM = Mathf.Lerp(currentRPM, Mathf.Max(idleRPM - 600, wheelRPM), Time.deltaTime * 3f);
+            torque = (hpToCurrentRPMCurve.Evaluate(currentRPM / redLineRPM) * enginePower / currentRPM) * gearRatios[currentGear] * carDifferentialRatio * 5252f * carClutch;
+        }
+        return torque;
+    }
+
     void GetInputs()
     {
+        carClutch = Input.GetKey(KeyCode.LeftShift) ? 0 : Mathf.Lerp(carClutch, 1, Time.deltaTime);
         gasInput = Input.GetAxis("Vertical");
         steeringInput = Input.GetAxis("Horizontal");
         slipAngle = Vector3.Angle(transform.forward, carRigidBody.velocity - transform.forward);
