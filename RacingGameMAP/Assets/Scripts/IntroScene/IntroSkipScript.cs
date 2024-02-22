e2c765aad037953bfc9cfb58fecce3aee0c422dd
Ridanix <marekmm04@gmail.com>
2024-02-23 00:43:45 +0100

    IntroInGame
    FixedItALittle
    More StuffCommingTommorow

--- RacingGameMAP/Assets/Scripts/IntroScene/IntroSkipScript.cs
+++ RacingGameMAP/Assets/Scripts/IntroScene/IntroSkipScript.cs
@@ -2,23 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 
 public class IntroSkipScript : MonoBehaviour
 {
+    public VideoPlayer videoPlayer;
     // Start is called before the first frame update
     void Start()
     {
-    
+    videoPlayer.loopPointReached += OnIntroFinished;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(2);
         }
+    }
+    void OnIntroFinished(VideoPlayer vp){
+        SceneManager.LoadScene(2);
     }
 }
